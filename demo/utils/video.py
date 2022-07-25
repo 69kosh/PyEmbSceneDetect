25da641952ea51a949cc7e19d8e23e9fe6fe2e89
69kosh <69kosh@gmail.com>
2022-07-25 12:00:31 +0300

    fix image preprocessing bug

--- demo/utils/video.py
+++ demo/utils/video.py
@@ -1,3 +1,4 @@
+from math import ceil
 import ffmpeg
 import cv2
 import numpy as np
@@ -7,16 +8,31 @@ from pytube import YouTube
 
 def prepareGeneratorFromVideo(filename: str, shape: tuple[int, int] = (160, 160), every: int = 1):
 
+
+    probe = ffmpeg.probe(filename)
+    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+    width = int(video_stream['width'])
+    height = int(video_stream['height'])
+
+    # maxSize = max(width, height)
+    ratio = max(shape[0], shape[1]) / max(width, height)
+
+    width = ceil(width * ratio)
+    height = ceil(height * ratio)
+
+    # print(ratio, width, height)
+
     process = (
         ffmpeg
         .input(filename)
-        .output('pipe:', format='rawvideo', pix_fmt='bgr24', s='{}x{}'.format(shape[0], shape[1]))
+        .filter('scale', max(shape[0],shape[1]), -1)
+        .output('pipe:', format='rawvideo', pix_fmt='rgb24') # pix_fmt='bgr24',, s='{}x{}'.format(shape[0], shape[1])
         .run_async(pipe_stdout=True)
     )
 
     i = 0
     while True:
-        in_bytes = process.stdout.read(shape[0] * shape[1] * 3)
+        in_bytes = process.stdout.read((width * height * 3))
         if not in_bytes:
             break
 
@@ -24,8 +40,14 @@ def prepareGeneratorFromVideo(filename: str, shape: tuple[int, int] = (160, 160)
         if ((i - 1) % every) != 0:
             continue
 
-        image = np.frombuffer(in_bytes, np.uint8).reshape(
-            [shape[0], shape[1], 3])
+        buffer = np.frombuffer(in_bytes, np.uint8).reshape((height, width, 3))
+
+        image = np.zeros([shape[0], shape[1], 3])
+        start = ((shape[0] - height) // 2)
+
+        image[start:start + height] = buffer
+
+        image *= 1.0/255
         # cv2.imshow('image', image)
         # cv2.waitKey(1)
 
@@ -34,11 +56,11 @@ def prepareGeneratorFromVideo(filename: str, shape: tuple[int, int] = (160, 160)
 
 def generateDemoVideo(inputFilename: str, outputFilename: str, scenes: list, every = 1):
 
-    frames = []
+    frames = {}
 
     for s, scene in enumerate(scenes):
         for i in range(scene[0] * every, (scene[1] + 1) * every):
-            frames.append((s, scene))
+            frames[i] = (s, scene)
 
     probe = ffmpeg.probe(inputFilename)
     video_stream = next(
@@ -75,33 +97,50 @@ def generateDemoVideo(inputFilename: str, outputFilename: str, scenes: list, eve
             .reshape([height, width, 3])
         )
 
-        s = frames[i][0]
-        e = frames[i][1][1]
+        if i in frames:
+            s = frames[i][0]
+            e = frames[i][1][1]
 
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        bottomLeftCornerOfText = (10, 40)
-        fontScale = 1
-        fontColor = (255, 255, 255)
-        thickness = 2
-        lineType = 2
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            bottomLeftCornerOfText = (10, 40)
+            fontScale = 1
+            fontColor = (64, 64, 64)
+            thickness = 6
+            lineType = 2
 
-        cv2.putText(in_frame, 'scene {s:03d}: {i:06d} -> {n:06d}'.format(s=s + 1, i=i + 1, n=e * every + 1),
-                    bottomLeftCornerOfText,
-                    font,
-                    fontScale,
-                    fontColor,
-                    thickness,
-                    lineType)
+            cv2.putText(in_frame, 'scene {s:03d}: {i:06d} -> {n:06d}'.format(s=s + 1, i=i + 1, n=e * every + 1),
+                        bottomLeftCornerOfText,
+                        font,
+                        fontScale,
+                        fontColor,
+                        thickness,
+                        lineType)
 
-        out_frame = in_frame 
+
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            bottomLeftCornerOfText = (10, 40)
+            fontScale = 1
+            fontColor = (255, 255, 255)
+            thickness = 2
+            lineType = 2
+
+            cv2.putText(in_frame, 'scene {s:03d}: {i:06d} -> {n:06d}'.format(s=s + 1, i=i + 1, n=e * every + 1),
+                        bottomLeftCornerOfText,
+                        font,
+                        fontScale,
+                        fontColor,
+                        thickness,
+                        lineType)
+
+            out_frame = in_frame 
+
+            process2.stdin.write(
+                out_frame
+                .astype(np.uint8)
+                .tobytes()
+            )
 
         i += 1
-
-        process2.stdin.write(
-            out_frame
-            .astype(np.uint8)
-            .tobytes()
-        )
 
     process2.stdin.close()
     process1.wait()
